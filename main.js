0e89fa90aa637cfe7b5178c6d815de6404340aa0
Alexandre Ribeiro <89461762+alexandreribeiro7@users.noreply.github.com>
2022-08-29 21:41:43 -0300

    This is my personal Web-Site

---
+++ main.js
@@ -0,0 +1,58 @@
+const el = document.querySelector("#text");
+const text = "LINGUAGENS";
+const interval = 150;
+
+function showText(el, text, interval) {
+
+    const char = text.split("").reverse();
+    const type = setInterval(() => {
+        
+        if(!char.length) {
+            return clearInterval(type);
+        }
+
+        const next = char.pop();
+
+        el.innerHTML += next;
+
+    }, interval);
+}
+
+showText(el, text, interval);
+
+const ek = document.querySelector("#blog_post_title");
+const blog_post_title = "SOBRE O AUTOR";
+const Interval = 150;
+
+function showText(el, blog_post_title, interval) {
+
+    const char = blog_post_title.split("").reverse();
+    const type = setInterval(() => {
+        
+        if(!char.length) {
+            return clearInterval(type);
+        }
+
+        const next = char.pop();
+
+        el.innerHTML += next;
+
+    }, interval);
+}
+
+showText(ek, blog_post_title, interval);
+
+//Particles.js
+
+var particles = Particles.init({
+	selector: '.background',
+  color: ['#ffffff', '#ffffff'],
+  connectParticles: true,
+  responsive: [{
+  	breakpoint: 800,
+    options: {
+    	maxParticles: 50,
+      connectParticles: true
+    }
+  }]
+});
